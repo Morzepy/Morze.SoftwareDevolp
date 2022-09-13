416d6715fb9605a92dce1bade37c7896d83aaaee
Morze_sharp <andpowec@mail.ru>
2022-09-13 11:19:45 +0300

    изменение файлов

--- Morze.SoftwareDevolpConsole/Program.cs
+++ Morze.SoftwareDevolpConsole/Program.cs
@@ -8,7 +8,7 @@ namespace Morze.SoftwareDevolp.SoftwareDevolpConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!{}");
         }
     }
 }
